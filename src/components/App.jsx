af0301a64b0b40d14d8ab432ffdd4ba8c751a1a9
andrew morales <andrew.morales725@gmail.com>
2021-11-17 14:22:47 -0500

    Added MusicTable component, Updated App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import MusicTable from './MusicTable/MusicTable';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-
+            songs: []
         }
     }
+    componentDidMount(){
+        this.getSongList();
+    }
+
+    async getSongList(){
+        let response = await axios.get('http://127.0.0.1:8000/music/');
+        this.setState({
+            songs: response.data
+        })
+    }
+
     render() { 
         return ( 
-            <h1>Display</h1>
+            <div>
+                <MusicTable songs={this.state.songs}/>    
+            </div>
         );
     }
 }
---
+++ src/components/MusicTable/MusicTable.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function MusicTable(props) {
+    return ( 
+        <div>
+            <table>
+                <thead>
+                <tr>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Genre</th>
+                <th>Release Date</th>
+                </tr>
+                </thead>
+                <tbody>
+                    {props.songs.map(songs => (
+                        <tr key={songs.id}>
+                            <td >{songs.id}</td>
+                            <td >{songs.title}</td>
+                            <td >{songs.artist}</td>
+                            <td >{songs.album}</td>
+                            <td >{songs.genre}</td>
+                            <td >{songs.release_date}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+     );
+}
+
+export default MusicTable;
