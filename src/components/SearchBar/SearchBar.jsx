cf43c811c190727f296c9f7a9540099d03845374
andrew morales <andrew.morales725@gmail.com>
2021-11-18 17:36:40 -0500

    Reset feature completed

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,38 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { }
-    }
+const SearchBar = (props) => {
 
-    handleSubmit = (event) => {
+    let handleSubmit = (event) => {
         event.preventDefault();
         alert('search');
         let searchBy = document.getElementById('searchBy').value;
         let searchFor = document.getElementById('searchFor').value;
         console.log(searchBy);
         console.log(searchFor);
-        this.props.filteredSearch(searchBy, searchFor);
+        props.filteredSearch(searchBy, searchFor);
     }
 
-    render() { 
-        return ( 
-            <form onSubmit={(event) => this.handleSubmit(event)}>
+    const handleReset = () => {
+        props.getSongList();
+    }
+
+    return (
+        <form onSubmit={(event) => handleSubmit(event)} onReset={(event) => handleReset(event)}>
             <label>Search by: </label>
-            <select id="searchBy">
-                <option value="title">Title</option>
-                <option value="artist">Artist</option>
-                <option value="album">Album</option>
-                <option value="genre">Genre</option>
-                <option value="release_date">Release Date</option>
-            </select>
+                <select id="searchBy">
+                    <option value="title">Title</option>
+                    <option value="artist">Artist</option>
+                    <option value="album">Album</option>
+                    <option value="genre">Genre</option>
+                    <option value="release_date">Release Date</option>
+                </select>
             <label>For: </label>
             <input id="searchFor" type="text" />
             <button type="submit">Search</button>
+            <button type="reset">Reset</button>
         </form>
-        );
-    }
+            );
 }
  
 export default SearchBar;
